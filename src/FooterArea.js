9d49074d88b4b22c757d99da1bbb93aab0b40d32
ror-shubham <ror-shubham@github.com>
2017-09-06 02:35:16 +0530

    added skelton for search and footer

--- src/FooterArea.js
+++ src/FooterArea.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
- 
+import React, {Component} from 'react';
+import FontIcon from 'material-ui/FontIcon';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 
-var FooterArea = () => (
-  <RaisedButton label="Default" primary={true} />
-);
+const recentsIcon = <FontIcon className="material-icons">Home</FontIcon>;
+const favoritesIcon = <FontIcon className="material-icons">Tool</FontIcon>;
+const rankingsIcon = <FontIcon className="material-icons">Rankings</FontIcon>;
+const careersIcon = <FontIcon className="material-icons">Careers</FontIcon>;
+const faculty360 = <FontIcon className="material-icons">FACULTY360</FontIcon>;
+
+/**
+ * A simple example of `BottomNavigation`, with three labels and icons
+ * provided. The selected `BottomNavigationItem` is determined by application
+ * state (for instance, by the URL).
+ */
+class FooterArea extends Component {
+  state = {
+    selectedIndex: 0,
+  };
+
+  select = (index) => this.setState({selectedIndex: index});
+
+  render() {
+    return (
+      <Paper zDepth={1}>
+        <BottomNavigation selectedIndex={this.state.selectedIndex}>
+          <BottomNavigationItem
+            icon={faculty360}
+            onClick={() => this.select(0)}
+          />
+          <BottomNavigationItem
+            icon={recentsIcon}
+            onClick={() => this.select(1)}
+          />
+          <BottomNavigationItem
+            icon={favoritesIcon}
+            onClick={() => this.select(2)}
+          />
+          <BottomNavigationItem
+            icon={rankingsIcon}
+            onClick={() => this.select(3)}
+          />
+          <BottomNavigationItem
+            icon={careersIcon}
+            onClick={() => this.select(4)}
+          />
+        </BottomNavigation>
+      </Paper>
+    );
+  }
+}
 
 export default FooterArea;
--- src/SearchBarArea.js
+++ src/SearchBarArea.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import SearchBar from 'material-ui-search-bar'
  
 
 var SearchBarArea = () => (
-  <RaisedButton label="Default" primary={true} />
+	<div>
+		<h2>Find the right job, right now</h2>
+		<SearchBar
+			onChange={() => console.log('onChange')}
+			onRequestSearch={() => console.log('onRequestSearch')}
+			style={{
+			margin: '0 auto',
+			maxWidth: 800
+			}}
+		/>
+	</div>
 );
 
 export default SearchBarArea;
