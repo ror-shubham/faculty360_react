0a6b5eff09614cf931d00dffe3c037c887e82ad0
ror-shubham <ror-shubham@github.com>
2017-09-09 03:35:36 +0530

    login-signup button toggle

--- src/HeaderArea.js
+++ src/HeaderArea.js
@@ -10,41 +10,43 @@ const HeaderArea = React.createClass({
 	getInitialState: function(){
         return {
             login: '',
-            signup: 'btn-primary'
+            signup: 'btn-log'
         };
     },
 
     clickLogin: function(){
     	this.setState({
-    		login: 'btn-primary',
+    		login: 'btn-log',
             signup: ''
     	})
     },
     clickSignUp: function(){
     	this.setState({
     		login: '',
-            signup: 'btn-primary'
+            signup: 'btn-log'
     	})
     },
 	render: function () {
 		return(
-				<nav className="navbar" style={{margin:0,borderRadius:0,}}>
-				  <div className="container-fluid">
-				    <div className="navbar-header" style={{float:'left',}}>
-				      <a className="navbar-brand" href="#"><img src={logo} className="img-responsive" width="150" height="150"/></a>
-				    </div>
-				    <div className="btn-group" data-toggle="buttons" style={{float:'right',}}>
-			  	
-			  <label className={"btn btn-sm "+this.state.login} onClick={this.clickLogin.bind(this)}>
-			    <input type="radio" name="options" id="option1" autocomplete="off"/> LOG IN
-			  </label>
-			  <label className={"btn btn-sm " +this.state.signup} onClick={this.clickSignUp.bind(this)}>
-			    <input type="radio" name="options" id="option2" autocomplete="off" checked/> SIGN UP
-			  </label>
-			  
-			</div>
-				  </div>
-				</nav>
+			<nav className="navbar" style={{margin:0,borderRadius:0,}}>
+				<div className="container-fluid">
+					<div className="navbar-header" style={{float:'left',}}>
+						<a className="navbar-brand" href="#"><img src={logo} 
+							className="img-responsive" width="150" height="150"/>
+						</a>
+					</div>
+				<div className="btn-group" data-toggle="buttons" style={{float:'right',}}>
+
+					<label className={"btn btn-sm "+this.state.login} onClick={this.clickLogin.bind(this)}>
+						<input type="radio" name="options" id="option1" autocomplete="off"/> LOG IN
+					</label>
+					<label className={"btn btn-sm " +this.state.signup} onClick={this.clickSignUp.bind(this)}>
+						<input type="radio" name="options" id="option2" autocomplete="off" checked/> SIGN UP
+					</label>
+
+				</div>
+				</div>
+			</nav>
 			)
 	}
 });
